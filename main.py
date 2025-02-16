983dd1c420b341eb98031e498173b938f8810079
favtai <favtai11@gmail.com>
2025-02-16 13:39:55 +0100

    editted

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ async def predict(file: UploadFile = File(...)):
 
     return {"prediction": prediction}
 
-if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host="0.0.0.0")
+#if __name__ == "__main__":
+#    uvicorn.run(app, port=8080, host="0.0.0.0")
